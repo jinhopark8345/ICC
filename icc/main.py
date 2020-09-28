254500af13aec973d3be90faabad3e105a2372b5
jinho <jinhopark8345@gmail.com>
2020-09-28 17:56:34 +0900

    jsonschema works

--- icc/main.py
+++ icc/main.py
@@ -2,35 +2,30 @@ import jsonschema
 import json
 from jsonschema import validate
 
-# with open('ingredients_store.json') as json_file:
-#     data = json.load(json_file)
-#     gron = data['green onion']
-#     temp = gron['store method']
-#     # temp2 = data['quantity']
-#     print(temp)
-#     # print(temp2)
+ingredient_schema_path = './ingredient_store_schema.json'
 
-abs_path_to_schema = '/path/to/schema-doc-foobar.json'
-
-# Describe what kind of json you expect.
-studentSchema = {
-    "type": "object",
-    "properties": {
-        "name": {"type": "string"},
-        "rollnumber": {"type": "number"},
-        "marks": {"type": "number"},
-    },
-}
+with open(ingredient_schema_path, 'r') as f:
+    schema_data = f.read()
+ing_schema = json.loads(schema_data)
 
 def validateJson(jsonData):
     try:
-        validate(instance=jsonData, schema=studentSchema)
+        validate(instance=jsonData, schema=ing_schema)
     except jsonschema.exceptions.ValidationError as err:
         return False
     return True
 
 # Convert json to python object.
-jsonData = json.loads('{"name": "jane doe", "rollnumber": "25", "marks": 72}')
+
+# valid data with all properties
+jsonData = json.loads('{"name": "green onion", "store_method": "fridge", "expiration_duration": 3600, "image": "asdfklajsdfaj"}')
+
+# valid data without image data
+# jsonData = json.loads('{"name": "green onion", "store_method": "fridge", "expiration_duration": 3600}')
+
+# invalid data / number is string
+# jsonData = json.loads('{"name": "green onion", "store_method": "fridge", "expiration_duration": "3600"}')
+
 # validate it
 isValid = validateJson(jsonData)
 if isValid:
@@ -40,13 +35,13 @@ else:
     print(jsonData)
     print("Given JSON data is InValid")
 
-# Convert json to python object.
-jsonData = json.loads('{"name": "jane doe", "rollnumber": 25, "marks": 72}')
-# validate it
-isValid = validateJson(jsonData)
-if isValid:
-    print(jsonData)
-    print("Given JSON data is Valid")
-else:
-    print(jsonData)
-    print("Given JSON data is InValid")
+# # Convert json to python object.
+# jsonData = json.loads('{"name": "jane doe", "rollnumber": 25, "marks": 72}')
+# # validate it
+# isValid = validateJson(jsonData)
+# if isValid:
+#     print(jsonData)
+#     print("Given JSON data is Valid")
+# else:
+#     print(jsonData)
+#     print("Given JSON data is InValid")
