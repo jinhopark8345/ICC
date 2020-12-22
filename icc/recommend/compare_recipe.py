a750e847f16fa2ee98f4386e2f24fb113e059310
jinho <jinhopark8345@gmail.com>
2020-12-22 19:56:23 +0900

    clean up compare_recipe file

--- icc/recommend/compare_recipe.py
+++ icc/recommend/compare_recipe.py
@@ -1,11 +1,13 @@
-import datetime as dt
-from pytz import timezone, utc
-from iccdb.db_manage import *
-from iccjson.jconnect import *
+"""
+compare recpie API
+"""
+
+# from iccdb.db_manage import IccDB
+# from iccjson.jconnect import *
 
 
 def is_comparable(ask_ing, user_ing):
-    """tell if the ingredients are comparable
+  """tell if the ingredients are comparable
 
     Args:
         ask_ing: ingredient from recipe
@@ -16,16 +18,15 @@ def is_comparable(ask_ing, user_ing):
 
     """
 
-    if (ask_ing["name"]
-            == user_ing["name"]) and (ask_ing["quantity_unit"]
-                                      == user_ing["quantity_unit"]):
-        return True
+  if (ask_ing["name"] == user_ing["name"]) and (ask_ing["quantity_unit"]
+                                                == user_ing["quantity_unit"]):
+    return True
 
-    return False
+  return False
 
 
 def get_diff_ing(ask_ing, user_ing):
-    """compare two "same" ingredients
+  """compare two "same" ingredients
 
     Args:
         ask_ing: ingredient from recipe
@@ -35,21 +36,21 @@ def get_diff_ing(ask_ing, user_ing):
         return the dictionary of difference in terms of quantity
 
     """
-    if is_comparable(ask_ing, user_ing):
-        diff_quantity = ask_ing["quantity"] - user_ing["quantity"]
-        user_ing["quantity"] = diff_quantity
-        return user_ing
-        # return {
-        #     "name": ask_ing["name"],
-        #     "quantity": ask_ing["quantity"] - user_ing["quantity"],
-        #     "quantity_unit": ask_ing["quantity_unit"],
-        # }
-    else:
-        print("wrong use case")
+  if is_comparable(ask_ing, user_ing):
+    diff_quantity = ask_ing["quantity"] - user_ing["quantity"]
+    user_ing["quantity"] = diff_quantity
+    return user_ing
+    # return {
+    #     "name": ask_ing["name"],
+    #     "quantity": ask_ing["quantity"] - user_ing["quantity"],
+    #     "quantity_unit": ask_ing["quantity_unit"],
+    # }
+  else:
+    print("wrong use case")
 
 
 def get_need_ing(ask_ing, user_ings):
-    """how much more "ask_ingredient" does user need?
+  """how much more "ask_ingredient" does user need?
     compare "ask_ingredient" that user need from user_ingredients"
 
         Args:
@@ -64,17 +65,15 @@ def get_need_ing(ask_ing, user_ings):
 
     """
 
-    need_ing = next(
-        ((get_diff_ing(ask_ing, user_ing))
-         for user_ing in user_ings if is_comparable(user_ing, ask_ing)),
-        ask_ing,
-    )
+  need_ing = next(
+      ((get_diff_ing(ask_ing, user_ing))
+       for user_ing in user_ings if is_comparable(ask_ing, user_ing)), ask_ing)
 
-    return need_ing
+  return need_ing
 
 
 def get_need_ings(recipe, user_ings):
-    """find ingredinets that user need to make the recipe
+  """find ingredinets that user need to make the recipe
 
     Args:
        user_ings [] ingreidents that user has
@@ -84,15 +83,11 @@ def get_need_ings(recipe, user_ings):
         ingredients that user need to make the recipe
 
     """
-    need_user_ings = [
-        get_need_ing(cur_need_ing, user_ings)
-        for cur_need_ing in recipe["ings"]
-    ]
+  need_user_ings = [
+      get_need_ing(cur_need_ing, user_ings) for cur_need_ing in recipe["ings"]
+  ]
 
-    need_user_ings = [ing for ing in need_user_ings if ing["quantity"] > 0]
-    return need_user_ings
-
-
-# def get_need_ings2(recipe_name):
+  need_user_ings = [ing for ing in need_user_ings if ing["quantity"] > 0]
+  return need_user_ings
 
 
